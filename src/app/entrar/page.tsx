1c794641aca87e56554dc67ad57e60f875b7cba0
duruduhnunes <duruduh883@gmail.com>
2025-04-05 16:34:57 -0300

    create the login page

---
+++ src/app/entrar/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <div>Pagina de acessar a conta</div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import { LoginPage } from "@/components/login";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-     <h1>ola</h1>
+    <div>
+      <LoginPage />
     </div>
   );
 }
